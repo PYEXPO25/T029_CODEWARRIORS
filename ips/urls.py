3bf822069c456b00c879815726a0b1b0ed1bc7e6
sharini05 <sharini.s123@gmail.com>
2025-02-21 18:03:07 +0530

    initial

---
+++ ips/urls.py
@@ -0,0 +1,27 @@
+"""
+URL configuration for ips project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('reserve/<int:slot_id>/', views.reserve_parking, name='reserve_parking'),
+    path('payment/<int:reservation_id>/', views.make_payment, name='make_payment'),
+    path('available-slots/',views.available_slots, name='available_slots'),
+    path('check-expired/', views.check_expired_reservations, name='check_expired_reservations'),
+]
