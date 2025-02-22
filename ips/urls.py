1ea51ad209cb1d4a8b4f4377a6088abfbb67b63d
sharini05 <sharini.s123@gmail.com>
2025-02-22 16:38:43 +0530

    final

--- ips/urls.py
+++ ips/urls.py
@@ -1,21 +1,3 @@
-"""
-URL configuration for ips project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
-
 from slotfinder.urls import path,include
 from django.contrib import admin
 from . import views
@@ -26,7 +8,7 @@ urlpatterns = [
     path('available-slots/',views.available_slots, name='available_slots'),
     path('check-expired/', views.check_expired_reservations, name='check_expired_reservations'),
 ]
-# myproject/urls.py
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
--- slotfinder/urls.py
+++ slotfinder/urls.py
@@ -1,6 +1,8 @@
 from ips.urls import path
 from . import views
 from ips import views as iot_views
+from . import views
+
 
 urlpatterns = [
     path('reserve/<int:slot_id>/', views.reserve_parking, name='reserve_parking'),
@@ -11,4 +13,14 @@ urlpatterns = [
 
 urlpatterns = [
     path('', views.index, name='index'),
+
 ]
+def path():
+    path('find_parking/', views.find_parking_path, name='find_parking'),
+    path('book_parking/', views.book_parking, name='book_parking'),  #
+path()
+def include():
+   
+    path('parking/', include('slotfinder.urls')),  # Include parking system URLs
+    path('reservations/', include('reservations.urls')), 
+include()
